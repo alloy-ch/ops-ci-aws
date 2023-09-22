2c1aac814591199d0a3c06cb6667a0abc15a9bcc
Andrea Artaria <andrea.artaria@ringier.ch>
2023-09-22 16:09:02 +0200

    feat(lambda-py): give control over poetry groups

--- roles/build_lambda_function_py/files/docker_build_python.sh
+++ roles/build_lambda_function_py/files/docker_build_python.sh
@@ -22,12 +22,13 @@ pip --quiet --disable-pip-version-check --no-color install --no-cache-dir "poetr
 echo "Installing dependencies"
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true
-poetry install --without dev,from_lambda_layers,runtime --no-interaction --no-ansi --no-cache
+echo Installing dependencies, by running: 'poetry install "$@" --no-interaction --no-ansi --no-cache'
+poetry install "$@" --no-interaction --no-ansi --no-cache
 echo "Packing release files"
 cp --recursive --no-preserve=ownership .venv/lib/python*/site-packages/* /output/
 packages=$(python -c "import toml;file=open('pyproject.toml');print(','.join(x['include'] for x in toml.load(file)['tool']['poetry']['packages']));file.close()")
 for package in ${packages//,/ }
 do
-    cp --recursive --no-preserve=ownership ${package} /output/
+    cp --recursive --no-preserve=ownership "${package}" /output/
 done
 echo "all done!"
